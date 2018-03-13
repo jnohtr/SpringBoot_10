b7731212b60755a7c4a1c6fa81739ec3ad8322fb
GBTC441007UR <GBTC441007UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-03-13 13:08:40 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, Long> {
+    Object findOne(long id);
+
+    void delete(long id);
+}
